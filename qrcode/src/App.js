5a0549115b934a614eb7e9f15c2b8b581f5f417c
Kanya Zembe <zembekanya@gmail.com>
2023-08-15 17:42:38 +0200

    layout set up

--- qrcode/src/App.js
+++ qrcode/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import qrimage from "./images/image-qr-code.png";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+      <img src={qrimage} alt="" width={300} height={300} />
+      </>
+      <p>
+        <h1>Improve your front-end skills by building projects</h1>
+        <h4>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</h4>
+      </p>
+
     </div>
   );
 }
