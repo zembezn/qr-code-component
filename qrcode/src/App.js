c0005ab85571136b3d4ea6104a09477ff643a849
Kanya Zembe <zembekanya@gmail.com>
2023-08-18 11:58:28 +0200

    linked github profile

--- qrcode/src/App.js
+++ qrcode/src/App.js
@@ -17,7 +17,7 @@ function App() {
 
       <div className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="">Frontend Mentor</a>. 
-        Coded by <a href="#">Kanya Zembe</a>.
+        Coded by <a href="https://github.com/zembezn?tab=repositories">Kanya Zembe</a>.
       </div>
     </>
   );
