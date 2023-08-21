23b6d081e45b546c76d5650feaee1d8d7347884b
Kanya Zembe <141934752+zembezn@users.noreply.github.com>
2023-08-21 17:09:42 +0200

    qr code build code

---
+++ App.js
@@ -0,0 +1,26 @@
+import qrimage from "./images/image-qr-code.png";
+import GlobalStyles from "./Components/Styled/GlobalStyle";
+import { Card } from "./Components/Styled/Card.styled";
+
+
+
+function App() {
+  return (
+    <div>
+      <GlobalStyles />
+      
+      <Card>
+      <img src={qrimage} alt="qr code" />
+        <h2>Improve your front-end skills by building projects</h2>
+        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </Card>
+
+      <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="">Frontend Mentor</a>. 
+        Coded by <a href="https://github.com/zembezn?tab=repositories">Kanya Zembe</a>.
+      </div>
+    </div>
+  );
+}
+
+export default App;
