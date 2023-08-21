351a084b2357b3a0acb43aa4f53598b3f14e9e9d
Kanya Zembe <zembekanya@gmail.com>
2023-08-21 15:25:18 +0200

    updated font import to link in html

--- src/Components/Styled/GlobalStyle.js
+++ src/Components/Styled/GlobalStyle.js
@@ -2,9 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
 
-  @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap');
-
-* {
+  * {
     margin: 0;
     padding: 0;
 }
